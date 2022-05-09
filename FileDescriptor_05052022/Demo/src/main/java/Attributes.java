ed235e4e883de699e9da993c980142782eb7b976
anand-roy-kr <102013138+anand-roy-kr@users.noreply.github.com>
2022-05-09 19:00:56 +0530

    Add files via upload

---
+++ FileDescriptor_05052022/Demo/src/main/java/Attributes.java
@@ -0,0 +1,58 @@
+//public class Attributes {
+//    private String name;
+//    private Integer order;
+//    private Integer min_width;
+//    private Integer max_width;
+//    private String type;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Integer getOrder() {
+//        return order;
+//    }
+//
+//    public void setOrder(Integer order) {
+//        this.order = order;
+//    }
+//
+//    public Integer getMin_width() {
+//        return min_width;
+//    }
+//
+//    public void setMin_width(Integer min_width) {
+//        this.min_width = min_width;
+//    }
+//
+//    public Integer getMax_width() {
+//        return max_width;
+//    }
+//
+//    public void setMax_width(Integer max_width) {
+//        this.max_width = max_width;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Consumer1{" +
+//                "name='" + name + '\'' +
+//                ", order=" + order +
+//                ", min_width=" + min_width +
+//                ", max_width=" + max_width +
+//                ", type='" + type + '\'' +
+//                '}';
+//    }
+//}
--- FileDescriptor_05052022/Demo/src/main/java/LoadAsJavaObjectList.java
+++ FileDescriptor_05052022/Demo/src/main/java/LoadAsJavaObjectList.java
@@ -11,9 +11,7 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 import static java.lang.System.out;
 
@@ -25,20 +23,19 @@ public class LoadAsJavaObjectList {
         int ct = 0;
         Connection c;
         Statement stmt;
-        Collection<Consumer> fds = new ArrayList<Consumer>();
-
+        ArrayList<Consumer> alc = new ArrayList<>();
         //reading nielsen consumer yaml file
         try (InputStream in = LoadAsJavaObjectList.class.getResourceAsStream("/nielsen.yml")) {
             Consumers Consumers = yaml.loadAs(in, Consumers.class);
             for (Consumer Consumer : Consumers.getConsumers()) {
-                fds.add(Consumer);
-                //System.out.println(Consumer);
+                alc.add(Consumer);
             }
-            //collection of file descriptor objects
-            System.out.println(fds);
-            //number of attributes for a specific consumer
-            ct = fds.size();
-            System.out.println("Number of columns as mentioned in yml file = "+ct);
+        //array list of file descriptor objects
+        Collections.sort(alc, Comparator.comparing(Consumer::getOrder));
+        System.out.println(alc);
+        //number of attributes for a specific consumer
+        ct = alc.size();
+        System.out.println("Number of columns as mentioned in yml file = "+ct);
 
         //connecting to dummy db (local postgres)
         try{
@@ -51,16 +48,14 @@ public class LoadAsJavaObjectList {
             ResultSet rs = stmt.executeQuery("select * from nielsen_processed_data");
             ResultSetMetaData metadata = rs.getMetaData();
             System.out.println("Number of columns: " + metadata.getColumnCount());
-//            String line1[] = {metadata.getColumnName(1), metadata.getColumnName(2),metadata.getColumnName(3)};
+            String line1[] = {metadata.getColumnName(1), metadata.getColumnName(2),metadata.getColumnName(3)};
+            System.out.println(line1);
 
             //getting header details from consumer objects
             String line2[] = {Consumers.getConsumers().get(0).getName(), Consumers.getConsumers().get(1).getName(), Consumers.getConsumers().get(2).getName()};
 
-            CSVWriter writer = new CSVWriter(new FileWriter("C:\\Users\\ttc6493\\Downloads\\FileDescriptor_05052022\\Demo\\src\\main\\output_files\\output_nielsen.csv"));
-            //writer.writeNext(line2);
-            String data[] = new String[3];
-
-            //populating files
+            //generating files
+            //todo - have a dynamic file storing mechanism
             FileWriter myWriter;
             myWriter = new FileWriter("C:\\Users\\ttc6493\\Downloads\\FileDescriptor_05052022\\Demo\\src\\main\\output_files\\output_nielsen.txt");
 
@@ -71,51 +66,21 @@ public class LoadAsJavaObjectList {
             String formattedDate = date.format(DateTimeFormatter.ofPattern("MMddyyyy"));
             System.out.println("Current Date: "+formattedDate);
 
-            myWriter.write(rowct+"| "+formattedDate+"|");
+            myWriter.write(rowct+"|"+formattedDate+"|");
             myWriter.write("\n");
             while(rs.next()) {
-                for (Consumer fd : fds) {
-                    switch (fd.getOrder()) {
-                        case 1:
-                            myWriter.write(rs.getString(line2[0]));
-                            myWriter.write("|  ");
-                            data[0] = rs.getString(line2[0]);
-                            break;
-
-                        case 2:
-                            myWriter.write(rs.getString(line2[1]));
-                            myWriter.write("|  ");
-                            data[1] = rs.getString(line2[1]);
-                            break;
-
-                        case 3:
-                            myWriter.write(rs.getString(line2[2]));
-                            myWriter.write("|  ");
-                            data[2] = rs.getString(line2[2]);
-                            break;
-                    }
-                    }
+                for (int i=0; i<ct; i++){
+                    myWriter.write(rs.getString(alc.get(i).getName()));
+                    myWriter.write("|");
+                }
                     myWriter.write("\n");
-                    writer.writeNext(data);
             }
                 myWriter.close();
-                writer.flush();
-                System.out.println("Pipe delimited Txt file and csv file created.");
-
-
-
-
-
+                System.out.println("Pipe delimited Txt file created.");
         }catch(Exception e){
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
             }
-
-
-
         }
     }
 }
-
-
-
